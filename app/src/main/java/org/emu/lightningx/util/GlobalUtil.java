48566de80c9adbb87b83b78fcc8720dabe35e283
Dalton Smith <daltzsmith@gmail.com>
2023-04-12 19:49:25 -0400

    Fix bug where initial start date was of a different format than calendar

--- app/src/main/java/org/emu/lightningx/util/GlobalUtil.java
+++ app/src/main/java/org/emu/lightningx/util/GlobalUtil.java
@@ -7,6 +7,25 @@ public class GlobalUtil {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 
     public static String getCurrentDateFormatted() {
-        return LocalDateTime.now().format(formatter);
+        String date = LocalDateTime.now().format(formatter);
+        String[] splitDate = date.split("/");
+
+        // DateTimeFormatter doesn't support syntax where the first number in month/day is 0
+        // For example, there is no way to format 03/05/2020 into 3/5/2020
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < splitDate.length; i++) {
+            if (splitDate[i].charAt(0) == '0') {
+                builder.append(splitDate[i].charAt(1));
+            } else {
+                builder.append(splitDate[i]);
+            }
+
+            // Don't append / if we're on the last substr
+            if (i != splitDate.length - 1) {
+                builder.append("/");
+            }
+        }
+
+        return builder.toString();
     }
 }
